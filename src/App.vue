551f73268b1ad78cc4e2145c175881e93aaeecb0
CaptainBullseye <dennisvulders@hotmail.com>
2022-06-15 18:33:45 +0200

    Windows-push-notification

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>   
+    <img alt="Peaks logo" src="./assets/PeaksTextBlack.png">
+    <div class="container">
+      <button @click="showNotification">Notification button</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      mm: 0,
+    }
+  },
+  mounted() {
+  // this.checkperm()
+},
+  methods: {
+    showNotification() {
+      this.mm = Math.floor(Math.random()*100);
+      new Notification("New message from Peaks!", {
+        body: "Hey Dennis!\nIk zag dat je een stresslevel van " + this.mm + " Hebt",
+        icon: "https://i.imgur.com/DGlDio3.png",
+      });
+      console.log(Notification.permission)
+    },
+    // checkperm() {
+    //   if (Notification.permission === "granted") {
+    //     showNotification();
+    //     } else if (Notification.permission !== "denied") {
+    //     Notification.requestPermission().then(permission => {
+    //       if (permission === "granted") {
+    //         showNotification();
+    //       }
+    //     })
+    //   }
+    // },
+  }  
 }
 </script>
 
@@ -22,5 +49,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.container {
+  text-align: center;
+  margin-top: 100px;
 }
 </style>
